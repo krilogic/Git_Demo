c3f7a63a92e1adcf3d4994050f245a9a9dcec73d
krilogic <krilogic@gmail.com>
2020-07-27 21:11:43 +0530

    From TeamB Add Controller

---
+++ Controllers/FromTeamBController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Git_Demo.Controllers
+{
+    public class FromTeamBController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
